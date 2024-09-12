0816dfde163c48be96246f2099c7089d84802c31
chlor17 <lortie.chad@gmail.com>
2024-09-12 15:31:40 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function openTab(tabId) {
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";  
+    }
+    
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(tabId).style.display = "block";  
+}
